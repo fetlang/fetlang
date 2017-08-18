892c0a8d9c8e4c50570cfce21e2cf3367ef4aefb
Property404 <martinez.dagan@gmail.com>
2017-08-17 22:19:55 -0400

    Asserts now display number of times called

--- fetishes/assertiveness/include/assert.h
+++ fetishes/assertiveness/include/assert.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef FETLANG_ASSERT
 #define FETLANG_ASSERT
+
 /* Throw runtime_error if condition is not met */
 void fetlang_assert(int condition, const char* message);
 #endif
--- fetishes/assertiveness/source/assert.c
+++ fetishes/assertiveness/source/assert.c
@@ -2,8 +2,12 @@
 #include "core/include/error.h"
 void fetlang_assert(int condition, const char* message)
 {
+	// Keeping track of times called makes this a bit more useful
+	static int times_called = 0;
 	if(!condition)
 	{
-		runtime_error("%s", message);
+		runtime_error("assertion %d(zero indexed): %s", times_called, message);
 	}
+	
+	times_called++;
 }
