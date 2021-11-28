21feca94fda2bfba9eb66a7bec34d91c3280bc15
Dagan Martinez <martinez.dagan@gmail.com>
2021-11-28 13:20:02 -0500

    Format python scripts

--- scripts/c_reference_make.py
+++ scripts/c_reference_make.py
@@ -4,17 +4,18 @@
 #
 # Requires Python >=3.6
 
-from common import*
+from common import *
 import shutil
 
+
 def makeDoxygenConfigFiles():
-	if not os.path.exists("./doxygen"):
-		os.makedirs("./doxygen")
-	fetishes = getListOfFetishes()
-	tabs = "\t"*4
-	for fetish in fetishes:
-		name = f"{fetish}_c_reference"
-		config_text=f"""PROJECT_NAME{tabs}= {name}
+    if not os.path.exists("./doxygen"):
+        os.makedirs("./doxygen")
+    fetishes = getListOfFetishes()
+    tabs = "\t" * 4
+    for fetish in fetishes:
+        name = f"{fetish}_c_reference"
+        config_text = f"""PROJECT_NAME{tabs}= {name}
 HIDE_UNDOC_MEMBERS{tabs}= YES
 OUTPUT_DIRECTORY{tabs}= ./doxygen/output
 WARNINGS{tabs}= YES
@@ -24,32 +25,36 @@ GENERATE_XML{tabs}= YES
 XML_OUTPUT{tabs}= ./{name}_xml
 """
 
-		fp = open("doxygen/"+name, "w")
-		fp.write(config_text)
-		fp.close()
+        fp = open("doxygen/" + name, "w")
+        fp.write(config_text)
+        fp.close()
+
 
 def runDoxygen():
-	files = [x[2] for x in os.walk("./doxygen")][0]
-	print(files)
+    files = [x[2] for x in os.walk("./doxygen")][0]
+    print(files)
 
-	for fname in files:
-		os.system("doxygen ./doxygen/"+fname)
+    for fname in files:
+        os.system("doxygen ./doxygen/" + fname)
+
+
 def runMoxygen():
-	fetishes = getListOfFetishes()
-	for fetish in fetishes:
-		filepath = f"../docs/c_reference/{fetish}.md"
-		os.system(f"moxygen ./doxygen/output/{fetish}_c_reference_xml -o {filepath}")
-		# Remove `public, cause this ain't C++
-		doc = open(filepath, "r");
-		new_contents = doc.read().replace("`public ", "`")
-		doc.close()
-		
-		doc = open(filepath, "w")
-		doc.write(new_contents)
-		doc.close()
+    fetishes = getListOfFetishes()
+    for fetish in fetishes:
+        filepath = f"../docs/c_reference/{fetish}.md"
+        os.system(f"moxygen ./doxygen/output/{fetish}_c_reference_xml -o {filepath}")
+        # Remove `public, cause this ain't C++
+        doc = open(filepath, "r")
+        new_contents = doc.read().replace("`public ", "`")
+        doc.close()
+
+        doc = open(filepath, "w")
+        doc.write(new_contents)
+        doc.close()
+
 
 if __name__ == "__main__":
-	makeDoxygenConfigFiles()
-	runDoxygen()
-	runMoxygen()
-	shutil.rmtree("./doxygen")
+    makeDoxygenConfigFiles()
+    runDoxygen()
+    runMoxygen()
+    shutil.rmtree("./doxygen")
--- scripts/fetlang_reference_make.py
+++ scripts/fetlang_reference_make.py
@@ -5,96 +5,110 @@
 # Requires Python >=3.6
 
 import json
-from common import*
+from common import *
+
 
 def getReferenceDirectory():
-	return getDocsDirectory()+"/reference/"
+    return getDocsDirectory() + "/reference/"
+
+
 def getContentsPath():
-	return getDocsDirectory()+"/reference.md"
+    return getDocsDirectory() + "/reference.md"
+
 
 def getCodePairPossibilities():
-	types = ["fraction", "chain", "stream", "reference"]
-	pairs = []
-	for a in types:
-		for b in types:
-			pairs.append(f"{a}/{b}")
-	return pairs
+    types = ["fraction", "chain", "stream", "reference"]
+    pairs = []
+    for a in types:
+        for b in types:
+            pairs.append(f"{a}/{b}")
+    return pairs
+
 
 def loadJsonFile(filename):
-	a = open(filename, "r")
-	j = json.loads(a.read())
-	a.close()
-	return j
+    a = open(filename, "r")
+    j = json.loads(a.read())
+    a.close()
+    return j
 
 
 def getMarkdownSection(fetish_file, section):
-	if section.replace(" ", "_") in fetish_file:
-		markdown = f"## {section.title()}\n"
-		for element in fetish_file[section.replace(" ", "_")]:
-			if "description" in element:
-				markdown += f"### {element['name']}\n"
-				if "description" in element:
-					markdown += element["description"]+"  \n\n"
-				if "grammars" in element:
-					markdown += "Grammar: "
-					for grammar in element["grammars"]:
-						markdown += grammar+ (", " if grammar != element["grammars"][-1] else "")
-					markdown += "  \n"
-				if "examples" in element:
-					markdown += "Examples:  \n"
-					for example in element["examples"]:
-						markdown += "`"+example+"`  \n\n"
-				if "type" in element:
-					markdown += f"Type: {element['type']}  \n"
-				if "gender" in element:
-					markdown += f"Gender: {element['gender']}  \n"
-				if "code" in element:
-					markdown += "C Code: "
-					if section == "builtins":
-						markdown += "`"+element["code"]+"`  \n\n"
-					else:
-						markdown += " \n\n"
-						for pair in getCodePairPossibilities():
-							if pair in element["code"]:
-								markdown += (f"    /* {pair} overload */\n    {element['code'][pair]}  \n\n")
-		return markdown
-	return ""
+    if section.replace(" ", "_") in fetish_file:
+        markdown = f"## {section.title()}\n"
+        for element in fetish_file[section.replace(" ", "_")]:
+            if "description" in element:
+                markdown += f"### {element['name']}\n"
+                if "description" in element:
+                    markdown += element["description"] + "  \n\n"
+                if "grammars" in element:
+                    markdown += "Grammar: "
+                    for grammar in element["grammars"]:
+                        markdown += grammar + (
+                            ", " if grammar != element["grammars"][-1] else ""
+                        )
+                    markdown += "  \n"
+                if "examples" in element:
+                    markdown += "Examples:  \n"
+                    for example in element["examples"]:
+                        markdown += "`" + example + "`  \n\n"
+                if "type" in element:
+                    markdown += f"Type: {element['type']}  \n"
+                if "gender" in element:
+                    markdown += f"Gender: {element['gender']}  \n"
+                if "code" in element:
+                    markdown += "C Code: "
+                    if section == "builtins":
+                        markdown += "`" + element["code"] + "`  \n\n"
+                    else:
+                        markdown += " \n\n"
+                        for pair in getCodePairPossibilities():
+                            if pair in element["code"]:
+                                markdown += f"    /* {pair} overload */\n    {element['code'][pair]}  \n\n"
+        return markdown
+    return ""
+
 
 def getReferencePageMarkdown(fetish_name):
-	fetish_file = loadJsonFile(getFetishFileOfFetish(fetish_name))
+    fetish_file = loadJsonFile(getFetishFileOfFetish(fetish_name))
 
-	markdown = f"# {fetish_name.title()}\n{fetish_file['description']}\n"
-	markdown += getMarkdownSection(fetish_file, "operators")
-	markdown += getMarkdownSection(fetish_file, "comparison operators")
-	markdown += getMarkdownSection(fetish_file, "builtins")
-	return markdown
+    markdown = f"# {fetish_name.title()}\n{fetish_file['description']}\n"
+    markdown += getMarkdownSection(fetish_file, "operators")
+    markdown += getMarkdownSection(fetish_file, "comparison operators")
+    markdown += getMarkdownSection(fetish_file, "builtins")
+    return markdown
+
 
 def makeReferencePage(fetish_name):
-	markdown = getReferencePageMarkdown(fetish_name)
-	filename = getReferenceDirectory()+fetish_name+".md"
-	fp = open(filename, "w")
-	fp.write(markdown)
-	fp.close()
-	return filename
-def getShortDescription(fetish):
-	descriptions = {"core" : "The included-by-default fetish that defines the language",
-	"assertiveness": "Contains assertion operators. Useful for unit tests",
-	"obedience": "File IO operators",
-	"manhandling": "Filesystem operators"}
+    markdown = getReferencePageMarkdown(fetish_name)
+    filename = getReferenceDirectory() + fetish_name + ".md"
+    fp = open(filename, "w")
+    fp.write(markdown)
+    fp.close()
+    return filename
 
-	return descriptions[fetish]
+
+def getShortDescription(fetish):
+    descriptions = {
+        "core": "The included-by-default fetish that defines the language",
+        "assertiveness": "Contains assertion operators. Useful for unit tests",
+        "obedience": "File IO operators",
+        "manhandling": "Filesystem operators",
+    }
+
+    return descriptions[fetish]
+
 
 def makeReferencePages():
-	contents = "# Fetish Reference\n"
-	for fetish in getListOfFetishes():
-		ref_page = makeReferencePage(fetish)
-		contents += f"""{fetish.title()} - {getShortDescription(fetish)}  
+    contents = "# Fetish Reference\n"
+    for fetish in getListOfFetishes():
+        ref_page = makeReferencePage(fetish)
+        contents += f"""{fetish.title()} - {getShortDescription(fetish)}  
 [C Reference](c_reference/{fetish}.md)  
 [Fetlang Reference](reference/{fetish}.md)  \n\n"""
-	fp = open(getContentsPath(), "w")
-	fp.write(contents)
-	fp.close()
-	
-	
+    fp = open(getContentsPath(), "w")
+    fp.write(contents)
+    fp.close()
+
+
 if __name__ == "__main__":
-	makeReferencePages()
+    makeReferencePages()
